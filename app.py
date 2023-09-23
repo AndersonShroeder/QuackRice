d36eeffc83f8e3792604a48656ec69c8e3791d27
Tigiy98 <anderson.schroeder0317@gmail.com>
2023-09-23 17:49:44 -0500

    Finished webapp class

--- app.py
+++ app.py
@@ -1,43 +1,69 @@
 import streamlit as st
 import pandas as pd
+from streamlit_timeline import st_timeline
+
+st.set_page_config(layout="wide")
+
+class WebApp:
+    def __init__(self):
+        self.col1, self.col2 = st.columns([.4, .6])
+        self.current_df = None
+        self.completed_df = None
+        self.task_columns = ["Task Name", "Task Priority", "Active", "Completed"]
 
 
-col1, col2 = st.columns([.5, .5])
+    def new_task_column(self):
+        with self.col1:
+            st.header("All Tasks")
+            priority_options = ["Very High", "High", "Medium", "Low", "Very Low"]
 
-if 'tasks' not in st.session_state:
-    st.session_state.tasks = pd.DataFrame(columns=["Task Name", "Task Priority", "Active", "Completed"])
+            task_name = st.text_input("Enter Task Name")
+            priority = st.selectbox("Select A Task Priority", priority_options)
 
-# Column containing Tasks
-with col1:
-    st.header("Tasks")
-    priority_options = ["Very High", "High", "Medium", "Low", "Very Low"]
+            if st.button("Add New Task"):
+                if task_name:
+                    st.session_state.tasks.loc[len(st.session_state.tasks)] = [task_name, priority, False, False]
 
-    task_name = st.text_input("Enter Task Name")
-    priority = st.selectbox("Select A Task Priority", priority_options)
+            edited_df = st.data_editor(st.session_state.tasks, num_rows = "dynamic",
+                                    column_config={"Active": st.column_config.CheckboxColumn("Active", default=False),
+                                                "Task Priority" : st.column_config.SelectboxColumn("Priority", options = priority_options)},
+                                        hide_index = True)
+            
 
-    edited_df = st.data_editor(st.session_state.tasks, 
-                               num_rows = "dynamic",
-                               column_config={"Active": st.column_config.CheckboxColumn("Active", default=False),
-                                              "Task Priority" : st.column_config.SelectboxColumn("Priority", options = priority_options)
-                                              },
-                                hide_index = True)
+            self.current_df = edited_df[edited_df['Active']]
+            self.completed_df = edited_df[edited_df['Completed']]
+
+    def active_task_column(self):
+        with self.col2:
+            current_col, completed_col = st.columns(2)
+
+            with current_col:
+                st.header("Active Tasks")
+                st.dataframe(self.current_df, hide_index = True)
+
+            with completed_col:
+                st.header("Completed Tasks")
+                st.dataframe(self.completed_df, hide_index = True)
     
-    if st.button("Add New Task"):
-        if task_name:
-            st.session_state.tasks.loc[len(st.session_state.tasks)] = [task_name, priority, False, False]
 
-    st.session_state.new_df = edited_df[edited_df['Active']]
-    st.write(st.session_state.new_df)
+    def generate_timeline_contents(self, task_dict):
+        items = []
+        id = 0
+        
+        for i in task_dict:
+            items.append({"id":id, "content": i, "start": task_dict[i][0], "end": task_dict[i][0]})
 
+        return items
+    
+    def plot_timeline(self, task_dict):
+        timeline = st_timeline(self.generate_timeline_contents(task_dict), groups=[], options={}, height="300px")
 
-# Column contained completed/current tasks and visual representation of schedule
-with col2:
-    current_col, completed_col = st.columns(2)
+    def run(self):
+        if 'tasks' not in st.session_state:
+            st.session_state.tasks = pd.DataFrame(columns=self.task_columns)
 
-    with current_col:
-        st.header("Current")
+        self.new_task_column()
+        self.active_task_column()
 
-    with completed_col:
-        st.header("Completed")
-
-    st.bar_chart()
+webapp = WebApp()
+webapp.run()
